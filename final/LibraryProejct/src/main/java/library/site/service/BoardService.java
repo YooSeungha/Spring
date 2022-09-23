5d76288bdd30e937d618ed8405d9168e842a4d18
Yoo Seung Ha <seungha06@nate.com>
2022-09-23 13:15:34 +0900

    finalProject

---
+++ final/LibraryProejct/src/main/java/library/site/service/BoardService.java
@@ -0,0 +1,16 @@
+package library.site.service;
+
+import java.util.List;
+
+import library.site.domain.Board;
+import library.site.domain.BoardListResult;
+
+public interface BoardService {
+	BoardListResult getBoardListResult(int cp, int ps);
+	BoardListResult getBoardListResult(int cp, int ps, String catgo, String keyword);
+	Board getBoard(long seq);
+	
+	void write(Board board);
+	void edit(Board board);
+	void remove(long seq);
+}
